3f2219a0893184b352330d5fb68e71331499f4f6
Zhaohan Wang <lkzhaohan@gmail.com>
2026-01-25 00:14:07 +0200

    Client-Side Pages, Components, and Interactivity
    HyperText Markup Language

--- walking-skeleton/client/src/routes/+page.svelte
+++ walking-skeleton/client/src/routes/+page.svelte
@@ -20,3 +20,11 @@
     <li>{todo.name}</li>
   {/each}
 </ul>
+
+<nav>
+  <ul>
+    <li><a href="/">Home</a></li>
+    <li><a href="/about">About</a></li>
+    <li><a href="/contact">Contact</a></li>
+  </ul>
+</nav>
---
+++ walking-skeleton/client/src/routes/about/+page.svelte
@@ -0,0 +1,9 @@
+<p>This is my about page!</p>
+
+<nav>
+  <ul>
+    <li><a href="/">Home</a></li>
+    <li><a href="/about">About</a></li>
+    <li><a href="/contact">Contact</a></li>
+  </ul>
+</nav>
